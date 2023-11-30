cdae0ee24678dca47529f96d19925a3c486b0ed1
mlnrengga <mlnrengga@gmail.com>
2023-11-30 17:55:09 +0700

    Percobaan 1

--- Percobaan115.java
+++ Percobaan115.java
@@ -6,7 +6,7 @@ public class Percobaan115 {
             return (n * faktorialRekursif(n - 1));
         }
     }
-    static int faktorialAlternatif (int n){
+    static int faktorialIteratif (int n){
         int faktor = 1;
         for (int i = n; i >= 1; i--){
             faktor = faktor * i;   
@@ -15,6 +15,6 @@ public class Percobaan115 {
     }
     public static void main(String[] args) {
         System.out.println(faktorialRekursif(5));
-        System.out.println(faktorialAlternatif(5));
+        System.out.println(faktorialIteratif(5));
     }
 }
