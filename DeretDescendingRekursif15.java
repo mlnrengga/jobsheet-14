05dac933af7c70ad64eaf35ba7a07461d78dd0e5
mlnrengga <mlnrengga@gmail.com>
2023-11-30 21:50:53 +0700

    Tugas No 1

---
+++ DeretDescendingRekursif15.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class DeretDescendingRekursif15 {
+    static void rekursif(int n) {
+        if (n <= 0) {
+            System.out.println(n);
+            return;
+        }
+        System.out.print(n + ", ");
+        rekursif(n - 1);
+    }
+    static void iteratif(int n) {
+        for (int i = n; i >= 0; i--) {
+            System.out.print(i);
+            if (i != 0)
+                System.out.print(", ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        rekursif(n);
+        iteratif(n);
+        sc.close();
+
+}
+}
